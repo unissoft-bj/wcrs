a3b20d4acd5c808855a3db241ac6690a3c67abec
michaelyin <gt8442b@yahoo.com>
2017-03-30 16:03:59 -0400

    add bonus model.

--- etc/wcrs.sql
+++ etc/wcrs.sql
@@ -28,7 +28,7 @@ CREATE TABLE if not exists user_info (
     create_t       datetime          DEFAULT NULL,	            #	记录时间
     modify_t       datetime          DEFAULT NULL,	            #	记录更新时间
     status         smallint          NOT NULL DEFAULT 0,
-    ticket         varchar(100)       DEFAULT NULL	            #
+    ticket         varchar(100)      DEFAULT NULL	            #
 )  DEFAULT CHARSET=utf8;
 
 ###############################################################
@@ -40,7 +40,60 @@ CREATE TABLE if not exists wc_event (
     from_user_name  varchar(30)        NOT NULL DEFAULT '',
     msg_type        varchar(20)        NOT NULL DEFAULT '',
     event           varchar(20)        NOT NULL DEFAULT '',
-    event_key       varchar(100)        NOT NULL DEFAULT '',
+    event_key       varchar(100)       NOT NULL DEFAULT '',
     create_t        datetime           DEFAULT NULL	            #	记录时间
 )  DEFAULT CHARSET=utf8;
+
+###############################################################
+# product
+###############################################################
+CREATE TABLE if not exists product (
+    id              int unsigned       NOT NULL auto_increment primary key,
+    description     varchar(30)        NOT NULL DEFAULT '',
+    p_type          varchar(30)        NOT NULL DEFAULT '',
+    create_t        datetime           DEFAULT NULL	            #	记录时间
+)  DEFAULT CHARSET=utf8;
+
+###############################################################
+# rule
+###############################################################
+CREATE TABLE if not exists rule (
+    id              int unsigned       NOT NULL auto_increment primary key,
+    first           smallint           NOT NULL DEFAULT 0,
+    second          smallint           NOT NULL DEFAULT 0,
+    third           smallint           NOT NULL DEFAULT 0,
+    root_p          smallint           NOT NULL DEFAULT 10,
+    root_id         varchar(32)        NOT NULL,
+    description     varchar(100)       NOT NULL DEFAULT '',
+    create_t        datetime           DEFAULT NULL	            #	记录时间
+)  DEFAULT CHARSET=utf8;
+
+###############################################################
+# product_rule
+###############################################################
+CREATE TABLE if not exists product_rule (
+    id             int unsigned        NOT NULL auto_increment primary key,
+    product_id     int unsigned        NOT NULL,
+    rule_id        smallint unsigned   NOT NULL,
+    create_t       datetime            DEFAULT NULL,
+    foreign key    (product_id)        references product (id),
+    foreign key    (rule_id)           references rule (id),
+    unique index   product_rule_idx1   (product_id, rule_id)
+)  DEFAULT CHARSET=utf8;
+
+###############################################################
+# user_product (? root id), open_id is unique across all public accounts?
+###############################################################
+CREATE TABLE if not exists user_product (
+    id             int unsigned        NOT NULL auto_increment primary key,
+    user_id        VARCHAR(36)         NOT NULL,
+    product_id     int unsigned        NOT NULL,
+    amount         numeric(15,2)       NOT NULL,
+    create_t       datetime            DEFAULT NULL,
+    foreign key    (user_id)           references user (id),
+    foreign key    (product_id)        references product (id),
+    unique index   user_product_idx1   (user_id, product_id)
+)  DEFAULT CHARSET=utf8;
+
+
 
