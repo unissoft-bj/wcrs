75883bcb873e972de430e26c23dceeade863b6fc
michaelyin <gt8442b@yahoo.com>
2017-04-12 17:55:22 -0400

    wechat menu classes.

---
+++ src/main/java/net/wyun/wcrs/wechat/menu/ComplexButton.java
@@ -0,0 +1,18 @@
+package com.dongyuan.weixin.menu;
+
+
+/**
+ * 
+ * @author qikuo
+ * @date 2017-2-28
+ */
+public class ComplexButton extends Button {
+	private Button[] sub_button;
+	public Button[] getSub_button() {
+		return sub_button;
+	}
+
+	public void setSub_button(Button[] sub_button) {
+		this.sub_button = sub_button;
+	}
+}
