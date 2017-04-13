feebbb4407568c206bc2e7ae9868b358ee97d099
michaelyin <gt8442b@yahoo.com>
2017-04-13 18:19:54 -0400

    clean up code

--- src/main/java/net/wyun/wcrs/controller/JingshujuController.java
+++ src/main/java/net/wyun/wcrs/controller/JingshujuController.java
@@ -3,12 +3,10 @@
  */
 package net.wyun.wcrs.controller;
 
-import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,11 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import net.wyun.wcrs.jsj.JSJFormInfo;
-import net.wyun.wcrs.model.User;
-import net.wyun.wcrs.model.UserRepository;
-import net.wyun.wcrs.model.UserStatus;
 import net.wyun.wcrs.service.JinShuJuHandler;
-import net.wyun.wcrs.service.SceneIdService;
 
 /**
  * @author Xuecheng
