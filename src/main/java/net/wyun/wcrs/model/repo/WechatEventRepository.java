d253a30814f382cfb47a11df8cecfad7e230182f
michaelyin <gt8442b@yahoo.com>
2017-04-14 15:52:24 -0400

    get pageable output for wechat events.

--- src/main/java/net/wyun/wcrs/model/repo/WechatEventRepository.java
+++ src/main/java/net/wyun/wcrs/model/repo/WechatEventRepository.java
@@ -4,6 +4,10 @@
 package net.wyun.wcrs.model.repo;
 
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import net.wyun.wcrs.model.WechatEvent;
@@ -13,5 +17,8 @@ import net.wyun.wcrs.model.WechatEvent;
  *
  */
 public interface WechatEventRepository extends CrudRepository<WechatEvent, Long>{
+
+	@Query(value="select evt from WechatEvent evt")
+	public List<WechatEvent> findWithPageable(Pageable pageable);
 	
 }
