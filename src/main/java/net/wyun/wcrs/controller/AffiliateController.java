51016e09465dc1ef4c77f10affa4633f5163f77c
michaelyin <gt8442b@yahoo.com>
2017-04-14 15:51:14 -0400

    crud of Affiliate.

--- src/main/java/net/wyun/wcrs/controller/AffiliateController.java
+++ src/main/java/net/wyun/wcrs/controller/AffiliateController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,6 +46,12 @@ private static final Logger logger = LoggerFactory.getLogger(UserController.clas
 	Iterable<Affiliate> getProducts(){
 		Iterable<Affiliate> itP = affiliateRepo.findAll();
 		return itP;
+	}
+	
+	@RequestMapping(value= "/affiliate/{aid}", method=RequestMethod.GET)
+	Affiliate getProduct(@PathVariable("aid") Long aid){
+		
+		return affiliateRepo.findOne(aid);
 	}
 
 }
