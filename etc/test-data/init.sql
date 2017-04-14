4e815e812830cd954519473023ccba90bc7145ac
michaelyin <gt8442b@yahoo.com>
2017-04-13 22:56:13 -0400

    add two more testing PA.

--- etc/test-data/init.sql
+++ etc/test-data/init.sql
@@ -1,5 +1,10 @@
-INSERT INTO public_account (p_a_id, app_id, app_secret, create_t)
-VALUES('gh_d7901caa4f9b', 'wx479cc0c5b93538df', '9cef9bc43e6ab4a2feedbfd3bf5b1dff', NOW());
+INSERT INTO public_account (p_a_id, app_id, app_secret, description, create_t)
+VALUES('gh_d7901caa4f9b', 'wx479cc0c5b93538df', '9cef9bc43e6ab4a2feedbfd3bf5b1dff', 'michaelyin testing', NOW());
 
-INSERT INTO public_account (p_a_id, app_id, app_secret, create_t)
-VALUES('gh_d7901caa4f9b', 'wx479cc0c5b93538df', '9cef9bc43e6ab4a2feedbfd3bf5b1dff', NOW());
+#qikuo
+INSERT INTO public_account (p_a_id, app_id, app_secret, description, create_t)
+VALUES('gh_d698ab97cae6', 'wx179e17d128a005d0', '64001733208cf2e39a1a1c9d6c14d52a', '人才招聘', NOW());
+
+#企管沙龙
+INSERT INTO public_account (p_a_id, app_id, app_secret, description, create_t)
+VALUES('gh_39bf79a08057', 'wxd72af67620853d7f', 'ef8337372b363be11276ff0a9a107390', '企管沙龙', NOW());
