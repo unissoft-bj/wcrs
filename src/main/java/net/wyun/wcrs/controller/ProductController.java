611d4b7afa907b7d58db7f65ff0b41fb4cedfcfd
michaelyin <gt8442b@yahoo.com>
2017-04-14 15:51:44 -0400

    crud of Product

--- src/main/java/net/wyun/wcrs/controller/ProductController.java
+++ src/main/java/net/wyun/wcrs/controller/ProductController.java
@@ -3,12 +3,15 @@
  */
 package net.wyun.wcrs.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,6 +38,23 @@ private static final Logger logger = LoggerFactory.getLogger(UserController.clas
 	Product saveProduct(@RequestBody Product p){
 		p.setCreateT(new Date());
 		return productRepo.save(p);
+	}
+	
+	@RequestMapping(value= "/product", method=RequestMethod.GET)
+	List<Product> getProducts(){
+		Iterable<Product> itP = productRepo.findAll();
+		List<Product> target = new ArrayList<Product>();
+		//itP.forEach(target::add);
+		for(Product p:itP){
+			target.add(p);
+		}
+		return target;
+	}
+	
+	@RequestMapping(value= "/product/{pid}", method=RequestMethod.GET)
+	Product getProduct(@PathVariable("pid") Long pid){
+		
+		return productRepo.findOne(pid);
 	}
 
 }
