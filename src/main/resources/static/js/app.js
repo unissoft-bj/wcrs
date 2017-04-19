f952ac3a11ea0bcd9d1ab02f1eea59a1165f21df
michaelyin <gt8442b@yahoo.com>
2017-04-19 10:30:20 -0400

    remove front end code for now.

--- src/main/resources/static/js/app.js
+++
@@ -1,9 +0,0 @@
-axios.get('http://59.110.113.110/secure/user/oEZJk0VlOT4P1x4647bzWS8YdbDg', {})
-        .then(function (res) {
-          document.getElementById('headimgurl').src = res.data.headimgurl;
-          document.getElementById('nickName').innerHTML = res.data.nickName;
-          document.getElementById('province').innerHTML = res.data.province;
-          document.getElementById('city').innerHTML = res.data.city;
-          document.getElementById('QrcodeUrl').src= 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket='+res.data.ticket;
-          document.getElementById('qrcode').style.background= "url('https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket='+res.data.ticket)";
-          });
