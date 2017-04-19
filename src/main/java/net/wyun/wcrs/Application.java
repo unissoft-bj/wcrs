e75be348565c2fb35135a63ea13420a1d4088064
michaelyin <gt8442b@yahoo.com>
2017-04-19 10:00:56 -0400

    add bean to log request.

--- src/main/java/net/wyun/wcrs/Application.java
+++ src/main/java/net/wyun/wcrs/Application.java
@@ -1,9 +1,13 @@
 package net.wyun.wcrs;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
@@ -18,4 +22,13 @@ public class Application extends SpringBootServletInitializer{
     }
 
     private static Class<Application> applicationClass = Application.class;
+    
+    @Bean
+    public Filter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(5120);
+        return filter;
+    }
 }
