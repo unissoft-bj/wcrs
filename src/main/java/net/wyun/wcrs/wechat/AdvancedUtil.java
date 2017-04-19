f5f6b9a5b5f8b18054c559e12f9013787ac51ea1
michaelyin <gt8442b@yahoo.com>
2017-04-19 09:59:47 -0400

    refactor code doing oauth with wechat.

--- src/main/java/net/wyun/wcrs/wechat/AdvancedUtil.java
+++ src/main/java/net/wyun/wcrs/wechat/AdvancedUtil.java
@@ -306,7 +306,6 @@ public class AdvancedUtil {
 	 * @return WeixinAouth2Token
 	 */
 	public static WeixinOauth2Token getOauth2AccessToken(String appId, String appSecret, String code) {
-		WeixinOauth2Token wat = null;
 		// 拼接请求地址
 		String requestUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
 		requestUrl = requestUrl.replace("APPID", appId);
@@ -314,21 +313,16 @@ public class AdvancedUtil {
 		requestUrl = requestUrl.replace("CODE", code);
 		// 获取网页授权凭证
 		JSONObject jsonObject = CommonUtil.httpsRequest(requestUrl, "GET", null);
-		if (null != jsonObject) {
-			try {
-				wat = new WeixinOauth2Token();
-				wat.setAccessToken(jsonObject.getString("access_token"));
-				wat.setExpiresIn(jsonObject.getInt("expires_in"));
-				wat.setRefreshToken(jsonObject.getString("refresh_token"));
-				wat.setOpenId(jsonObject.getString("openid"));
-				wat.setScope(jsonObject.getString("scope"));
-			} catch (Exception e) {
-				wat = null;
-				int errorCode = jsonObject.getInt("errcode");
-				String errorMsg = jsonObject.getString("errmsg");
-				log.error("获取网页授权凭证失败 errcode:{} errmsg:{}", errorCode, errorMsg);
-			}
+		if(null == jsonObject){
+			throw new RuntimeException("cannot pass wechat authorization !!!");
 		}
+		WeixinOauth2Token wat = new WeixinOauth2Token();
+		wat.setAccessToken(jsonObject.getString("access_token"));
+		wat.setExpiresIn(jsonObject.getInt("expires_in"));
+		wat.setRefreshToken(jsonObject.getString("refresh_token"));
+		wat.setOpenId(jsonObject.getString("openid"));
+		wat.setScope(jsonObject.getString("scope"));
+		
 		return wat;
 	}
 
