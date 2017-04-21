0d3eb130f09f1e34ca2fcbcd69faf4e831a0aec9
michaelyin <gt8442b@yahoo.com>
2017-04-21 15:58:38 -0400

    add firstname, lastname, and phone.

--- src/main/java/net/wyun/wcrs/model/Account.java
+++ src/main/java/net/wyun/wcrs/model/Account.java
@@ -12,14 +12,34 @@ import java.util.Set;
 @Entity
 @Table(name = "account")
 public class Account {
+	
+	public Account() {	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+	
     private String username;
     private String password;
+    
+    @Transient
     private String passwordConfirm;
+    
+    @Column(name="first_name")
+    private String firstName;
+    
+    @Column(name="last_name")
+    private String lastName;
+    
+    private String phone;
+    
+    private boolean enabled;
+    
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    
     public Long getId() {
         return id;
     }
@@ -53,8 +73,7 @@ public class Account {
         this.passwordConfirm = passwordConfirm;
     }
 
-    @ManyToMany
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    
     public Set<Role> getRoles() {
         return roles;
     }
@@ -62,4 +81,39 @@ public class Account {
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
     }
+
+    @Column(name = "enabled")
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	
 }
