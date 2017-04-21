32cbe4408a8ee94b8fae5120ccb6e593a7e02b6a
michaelyin <gt8442b@yahoo.com>
2017-04-20 23:14:36 -0400

    add OAuth check logic for redirecting from wechat.

--- src/main/java/net/wyun/wcrs/controller/wechat/OAuthController.java
+++ src/main/java/net/wyun/wcrs/controller/wechat/OAuthController.java
@@ -3,49 +3,26 @@
  */
 package net.wyun.wcrs.controller.wechat;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.util.Map;
-import java.util.UUID;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-
-import net.wyun.wcrs.jsj.JSJFormInfo;
-import net.wyun.wcrs.model.Gender;
 import net.wyun.wcrs.model.PAUser;
-import net.wyun.wcrs.model.WCUser;
+import net.wyun.wcrs.model.PublicAccount;
 import net.wyun.wcrs.model.WCUserStatus;
-import net.wyun.wcrs.model.WechatEvent;
 import net.wyun.wcrs.model.repo.PAUserRepository;
-import net.wyun.wcrs.model.repo.WCUserRepository;
-import net.wyun.wcrs.model.repo.WechatEventRepository;
-import net.wyun.wcrs.service.TokenService;
+import net.wyun.wcrs.model.repo.PublicAccountRepository;
 import net.wyun.wcrs.wechat.AdvancedUtil;
-import net.wyun.wcrs.wechat.CommonUtil;
-import net.wyun.wcrs.wechat.MessageUtil;
-import net.wyun.wcrs.wechat.SignUtil;
-import net.wyun.wcrs.wechat.message.event.QRCodeEvent;
-import net.wyun.wcrs.wechat.message.req.ReqTextMessage;
-import net.wyun.wcrs.wechat.message.resp.RespTextMessage;
 import net.wyun.wcrs.wechat.po.SNSUserInfo;
 import net.wyun.wcrs.wechat.po.WeixinOauth2Token;
-import net.wyun.wcrs.wechat.po.WeixinUserInfo;
 
 /**
  * @author Xuecheng
@@ -61,13 +38,7 @@ public class OAuthController {
 	PAUserRepository paUserRepo;
 	
 	@Autowired
-	WCUserRepository userRepo;
-	
-	@Autowired
-	WechatEventRepository weRepo;
-	
-	@Autowired
-	TokenService tokenService;
+	PublicAccountRepository paRepo;
 	
 	private final static String DENIED = "authdeny";
 	@RequestMapping(value= "/oauth", method=RequestMethod.GET)
@@ -78,18 +49,29 @@ public class OAuthController {
 		request.setCharacterEncoding("utf-8");
 		
 		String code = request.getParameter("code");
-        String state = request.getParameter("state");
-        logger.info("code {} and state {}", code, state);
-        if(null == code){
-        	throw new RuntimeException("wechat authorization error, null value code returned!!!");
+        String appId = request.getParameter("state");
+        logger.info("code {} and state {}", code, appId); //state ==> appId
+        if(null == code || code.isEmpty()){
+        	throw new RuntimeException("wechat authorization error, null/empty code returned!!!");
         }
+        
+        if(null == appId || appId.isEmpty()){
+        	throw new RuntimeException("wechat authorization error, null/empty appId returned!!!");
+        }
+        List<PublicAccount> lpa = paRepo.findByAppId(appId);
+        if(lpa.isEmpty()){
+        	throw new RuntimeException("wechat authorization error, cannot find public account!!!");
+        }
+        
+        PublicAccount pa = lpa.get(0);
         if (!DENIED.equals(code)) {
         	logger.info("OAuth successful.");
         	
             // access_token verification to wechat server
         	WeixinOauth2Token weixinOauth2Token = null;
         	try{
-        		weixinOauth2Token = AdvancedUtil.getOauth2AccessToken("wx479cc0c5b93538df", "9cef9bc43e6ab4a2feedbfd3bf5b1dff", code);
+        		//"wx479cc0c5b93538df", "9cef9bc43e6ab4a2feedbfd3bf5b1dff"
+        		weixinOauth2Token = AdvancedUtil.getOauth2AccessToken(appId, pa.getAppSecret(), code);
         	}catch (Exception e) {
         		logger.error("获取网页授权凭证失败", e);
         		throw new RuntimeException("wechat authorization error, 获取网页授权凭证失败!!!");
@@ -106,7 +88,7 @@ public class OAuthController {
             
             request.setAttribute("openId", openId);
             request.setAttribute("snsUserInfo", snsUserInfo);
-            request.setAttribute("state", state);
+            request.setAttribute("state", appId);
         }else{
         	throw new RuntimeException("wechat authorization error, authorization denied!!!");
         }
@@ -130,7 +112,6 @@ public class OAuthController {
         	String redirectUrl = "https://jinshuju.net/f/JN3P8g";
         	return "redirect:" + redirectUrl;
         }
-        
         
 	}
 	
