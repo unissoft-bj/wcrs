61d540ab4d1839cfc480d3af3401f9cadf5f6ccc
michaelyin <gt8442b@yahoo.com>
2017-04-20 23:11:56 -0400

    qinghuangdao rencaiku public account added.

--- etc/test-data/init.sql
+++ etc/test-data/init.sql
@@ -8,3 +8,7 @@ VALUES('gh_d698ab97cae6', 'wx179e17d128a005d0', '64001733208cf2e39a1a1c9d6c14d52
 #企管沙龙
 INSERT INTO public_account (p_a_id, app_id, app_secret, description, create_t)
 VALUES('gh_39bf79a08057', 'wxd72af67620853d7f', 'ef8337372b363be11276ff0a9a107390', '企管沙龙', NOW());
+
+#秦皇岛人才库
+INSERT INTO public_account (p_a_id, app_id, app_secret, description, create_t)
+VALUES('gh_d6f948a5a26b', 'wx67e331a601742a07', '4b21143254fcf59c1d09e67abe64a68a', '秦皇岛人才库', NOW());
