0d249ea97aad58a1e7486b70358bb43108ce6b86
michaelyin <gt8442b@yahoo.com>
2017-05-08 23:51:58 -0400

    wechat jssdk config added.

---
+++ src/main/java/net/wyun/wcrs/controller/wechat/JssdkController.java
@@ -0,0 +1,48 @@
+package net.wyun.wcrs.controller.wechat;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.wyun.wcrs.wechat.jssdk.JssdkConfig;
+import net.wyun.wcrs.wechat.jssdk.Message;
+ 
+/**
+ * ClassName: WeChatController
+ * @Description: 前端用户微信配置获取
+ * @author dapengniao
+ * @date 2016年3月19日 下午5:57:36
+ */
+@Controller
+@RequestMapping("/secure")
+public class JssdkController {
+	
+	@Autowired
+	JssdkConfig jssdkConfig;
+ 
+    /**
+     * @Description: 前端获取微信JSSDK的配置参数
+     * @param @param response
+     * @param @param request
+     * @param @param url
+     * @param @throws Exception
+     * @author dapengniao
+     * @date 2016年3月19日 下午5:57:52
+     */
+    @RequestMapping("/jssdk")
+    public Message JSSDK_config( @RequestParam(value = "url", required = true) String url,
+    		                     @RequestParam(value = "paId") String paId) {
+        try {
+            System.out.println(url);
+            Map<String, String> configMap = jssdkConfig.jsSDK_Sign(url, paId);
+            return Message.success(configMap);
+        } catch (Exception e) {
+            return Message.error();
+        }
+ 
+    }
+ 
+}
---
+++ src/main/java/net/wyun/wcrs/wechat/jssdk/JssdkConfig.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package net.wyun.wcrs.wechat.jssdk;
+
+import java.security.MessageDigest;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import net.wyun.wcrs.service.TokenService;
+
+/**
+ * @author michael
+ * 
+ *         /** ClassName: JSSDK_Config
+ * @Description: 用户微信前端页面的jssdk配置使用
+ * @author dapengniao
+ * @date 2016年3月19日 下午3:53:23
+ *
+ */
+@Component
+public class JssdkConfig {
+	
+	@Autowired
+	TokenService tokenService;
+
+	/**
+	 * @Description: 前端jssdk页面配置需要用到的配置参数
+	 * @param @return
+	 *            hashmap {appid,timestamp,nonceStr,signature}
+	 * @param @throws
+	 *            Exception
+	 * @author dapengniao
+	 * @date 2016年3月19日 下午3:53:23
+	 */
+	public HashMap<String, String> jsSDK_Sign(String url, String paId) throws Exception {
+		String nonce_str = create_nonce_str();
+		JSTicket ticket = tokenService.getTicket(paId);
+		
+		long timestamp = ticket.getTimeStamp();
+		String jsapi_ticket = ticket.getTicket();
+		// 注意这里参数名必须全部小写，且必须有序
+		String string1 = "jsapi_ticket=" + jsapi_ticket + "&noncestr=" + nonce_str + "&timestamp=" + timestamp + "&url="
+				+ url;
+		MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+		crypt.reset();
+		crypt.update(string1.getBytes("UTF-8"));
+		String signature = byteToHex(crypt.digest());
+		HashMap<String, String> jssdk = new HashMap<String, String>();
+		jssdk.put("appId", GlobalConstants.getInterfaceUrl("appid"));
+		jssdk.put("timestamp", "" + timestamp);
+		jssdk.put("nonceStr", nonce_str);
+		jssdk.put("signature", signature);
+		return jssdk;
+
+	}
+
+	private static String byteToHex(final byte[] hash) {
+		Formatter formatter = new Formatter();
+		for (byte b : hash) {
+			formatter.format("%02x", b);
+		}
+		String result = formatter.toString();
+		formatter.close();
+		return result;
+	}
+
+	private static String create_nonce_str() {
+		return UUID.randomUUID().toString();
+	}
+
+}
